4a79a2fa06332530edce62691e8652a26ad7c273
kimsiwoo <ksw98035213@gmail.com>
2024-08-13 15:37:40 +0900

    ADD :: Questionpage

---
+++ project/src/pages/Questionpage.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import { Header } from "../components/Header"
+
+export const Questionpage = () => {
+  return(
+    <>
+    <Header/>
+    <Conrainer>
+
+    </Conrainer>
+    </>
+  )
+}
+
+const Conrainer = styled.div`
+  box-sizing: border-box;
+  height: calc(100vh - 65px);
+  background-color: #f4f4f4;
+`
