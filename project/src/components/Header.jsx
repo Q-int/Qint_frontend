8d3b659820ac9a6d0b1db4ea1100f3f2865d2a6a
박지연 <163213733+j02on@users.noreply.github.com>
2024-08-14 16:35:55 +0900

    FEAT :: navigate 설정 및 로그인 버튼 클릭 시 모달 창 띄우기

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -1,21 +1,50 @@
-import styled from "styled-components";
-import { useLocation } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { useLocation, useNavigate } from "react-router-dom";
+import { LoginPage } from "../pages/LoginPage";
+import { useState } from "react";
 
-export const Header = ({path}) => {
+export const Header = () => {
+  const [modal, setModal] = useState(false);
+  const { pathname } = useLocation();
+
+  const navigate = useNavigate();
+
+  const MyPageClick = () => {
+    navigate("/mypage");
+  };
+
+  const LogoClick = () => {
+    navigate("/");
+  };
+
+  const LoginClick = () => {
+    setModal("true");
+  };
+
+  // if (modal == "true") {
+  // }
+
   return (
     <Container>
-      <Logo></Logo>
+      <Logo onClick={LogoClick}></Logo>
       <Buttons>
-        <Mypagebtn>마이페이지</Mypagebtn>
-        <Loginbtn>로그인</Loginbtn>
+        <Mypagebtn pathname={pathname} onClick={MyPageClick}>
+          마이페이지
+        </Mypagebtn>
+        <Loginbtn onClick={LoginClick}>로그인</Loginbtn>
       </Buttons>
+      {modal && (
+        <ModalOverlay onClick={LoginClick}>
+          <LoginPage />
+        </ModalOverlay>
+      )}
     </Container>
   );
 };
 
 const Container = styled.div`
   width: 100vw;
-  height: 70px;
+  height: 65px;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -27,6 +56,7 @@ const Logo = styled.img.attrs({
   src: "/images/Q-int.svg",
   alt: "로고",
 })`
+  cursor: pointer;
   height: 22px;
   margin-left: 40px;
 `;
@@ -37,11 +67,21 @@ const Buttons = styled.div`
   background-color: transparent;
 `;
 
-const Mypagebtn = styled.h1`
+const Mypagebtn = styled.div`
   background-color: transparent;
   font-size: 14px;
   font-weight: 400;
   cursor: pointer;
+  ${({ pathname }) =>
+    pathname.includes("mypage")
+      ? css`
+          color: #00eda6;
+          font-weight: 700;
+        `
+      : css`
+          color: #000000;
+          font-weight: 400;
+        `}
 `;
 
 const Loginbtn = styled.button`
@@ -59,4 +99,16 @@ const Loginbtn = styled.button`
     border: none;
     outline: none;
   }
+`;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
 `;
