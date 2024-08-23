8a810702ab6c9fae1680fff95d3b609713f312d7
kimsiwoo <ksw98035213@gmail.com>
2024-08-23 20:24:47 +0900

    FEAT :: Start component 추가

---
+++ project/src/components/Startbtn.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const Startbtn = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 200px;
+  height: 80px;
+  background-color: red;
+`
