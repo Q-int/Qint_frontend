8237eed5dff8083fb26e92336d440ebccc347ab2
박지연 <163213733+j02on@users.noreply.github.com>
2024-08-30 09:31:47 +0900

    FEAT :: 정규식

--- project/src/components/Regex.jsx
+++ project/src/components/Regex.jsx
@@ -1,1 +1,4 @@
-export const PasswordRegex = /^(?=.*[!@#$%^&*])(?=.{1,64}$).*/;
+export const PasswordRegex =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$/;
+
+export const EmailRegex = /^.+@([A-Za-z0-9-]+\.)+[A-Za-z]{2}[A-Za-z]*$/;
