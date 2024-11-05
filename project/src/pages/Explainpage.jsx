d6309c1ca2ffaed2a54e1a445b30ac9838a0fbc3
박지연 <163213733+j02on@users.noreply.github.com>
2024-11-05 20:01:40 +0900

    STYLE :: style 변경

--- project/src/pages/Explainpage.jsx
+++ project/src/pages/Explainpage.jsx
@@ -1,55 +1,51 @@
-import styled from "styled-components"
-import { Header } from "../components/Header"
-import { Nextbtn } from "../components/Nextbtn"
-import { Option } from "../components/Option"
-import { Explainbtn } from "../components/Explainbtn"
-import { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
+import styled from 'styled-components';
+import { Header } from '../components/Header';
+import { Nextbtn } from '../components/Nextbtn';
+import { Option } from '../components/Option';
+import { Explainbtn } from '../components/Explainbtn';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { ScoreCheckPage } from './ScoreCheckPage';
 
-export const Explainpage = () => {
-
+export const Explainpage = ({ qModalState, setQModalState }) => {
   const [Question, setQuestion] = useState({
-		"answer_text" : "3.1415926535...",
-		"commentay" : "어쩌구 저쩌구 해서 쨌든 그냥 니가 틀리고 내가 맞음 어쩔팁이",  
-		"is_correct" : true
-})
-const [Qnum, setQnum] = useState();
+    answer_text: "3.1415926535...",
+    commentay: "어쩌구 저쩌구 해서 쨌든 그냥 니가 틀리고 내가 맞음 어쩔팁이",
+    is_correct: true
+  });
+  const [Qnum, setQnum] = useState();
 
-const location = useLocation();
+  const location = useLocation();
 
-useEffect(() => {
-  setQuestion(location.state.EXQ);
-  setQnum(location.state.Qnum);
-})
+  useEffect(() => {
+    setQuestion(location.state.EXQ);
+    setQnum(location.state.Qnum);
+  });
 
-  return(
+  return (
     <Container>
-      <Header/>
+      <Header />
       <Contentboxcontainer>
-        <Contentbox>
-          {Question.commentay}
-        </Contentbox>
+        <Contentbox>{Question.commentay}</Contentbox>
         <QnumText>{Qnum + 1}/15</QnumText>
       </Contentboxcontainer>
       <Explaincontainer>
-        <Option option={Question.answer_text} rw={Question.is_correct}/>
-        <Explainbox>
-          그거 아닌데 뭔소리징
-        </Explainbox>
+        <Option option={Question.answer_text} rw={Question.is_correct} />
+        <Explainbox>그거 아닌데 뭔소리징</Explainbox>
       </Explaincontainer>
       <Buttoncontainer>
-        <Nextbtn/>
+        <Nextbtn qModalState={qModalState} setQModalState={setQModalState} />
       </Buttoncontainer>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   box-sizing: border-box;
   width: 100vw;
   background-color: #f4f4f4;
   height: 100vh;
-`
+`;
 
 const Contentboxcontainer = styled.div`
   box-sizing: border-box;
@@ -59,42 +55,42 @@ const Contentboxcontainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1.5vh
-`
+  gap: 1.5vh;
+`;
 
 const Contentbox = styled.div`
-width: 78vw;
-height: 15.7vh;
-background: #FFFFFF;
-box-shadow: inset 0px 0px 1.2vh rgba(103, 103, 103, 0.25);
-border-radius: 1vh;
-font-size: 2.75vh;
-display: flex;
-align-items: center;
-justify-content: center;
-margin-top: 3.2vh
-`
+  width: 78vw;
+  height: 15.7vh;
+  background: #ffffff;
+  box-shadow: inset 0px 0px 1.2vh rgba(103, 103, 103, 0.25);
+  border-radius: 1vh;
+  font-size: 2.75vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 3.2vh;
+`;
 
 const Explaincontainer = styled.div`
   box-sizing: border-box;
   height: calc(100vh - 8.9vh - 40vh);
   display: flex;
-  flex-direction: column; 
+  flex-direction: column;
   align-items: center;
   gap: 7vh;
-`
+`;
 
 const Explainbox = styled.div`
-box-sizing: border-box;
-width: 77.5vw;
-height: 31.5vh;
-padding: 2.7vh 1.6vw;
-background: #E4F9F3;
-box-shadow: inset 0px 0px 0.5vw rgba(103, 103, 103, 0.25);
-border-radius: 1vw;
-font-size: 2.8vh;
-font-weight: 400;
-`
+  box-sizing: border-box;
+  width: 77.5vw;
+  height: 31.5vh;
+  padding: 2.7vh 1.6vw;
+  background: #e4f9f3;
+  box-shadow: inset 0px 0px 0.5vw rgba(103, 103, 103, 0.25);
+  border-radius: 1vw;
+  font-size: 2.8vh;
+  font-weight: 400;
+`;
 
 const Buttoncontainer = styled.div`
   box-sizing: border-box;
@@ -103,11 +99,23 @@ const Buttoncontainer = styled.div`
   display: flex;
   justify-content: flex-end;
   padding-right: 11.7vw;
-`
+`;
 
 const QnumText = styled.p`
   margin: 0;
   font-size: 2vh;
-  color: #50555C;
+  color: #50555c;
   font-weight: 400;
+`;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
 `;
